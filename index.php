ca0e559e27b1a5c31b429c1c5dfd68416f1b0fab
yoanndelattre <yoanndelattre21@gmail.com>
2018-10-05 20:10:03 +0200

    add files

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Index of /</title>
+  </head>
+  <body>
+    <h1>Index of /</h1>
+	  <table>
+		  
+		  <tr><th valign="top"><img src="/img/blank.gif" alt="[ICO]"></th><th><a href="?C=N;O=D">Name</a></th><th><a href="?C=S;O=A">Size</a></th></tr>
+   		  <tr><th colspan="4"><hr></th></tr>
+		  
+		<tr><td valign="top"><img src="/img/back.gif" alt="[PARENTDIR]"></td><td><a href="/">Parent Directory</a></td><td>&nbsp;</td><td align="right">  - </td></tr>
+		  
+		<tr><td valign="top"><img src="/img/folder.gif" alt="[DIR]"></td><td><a href="/files/Projet Docker/">Projet Docker/</a></td><td align="right">  - </td></tr>
+		  
+	  </table>
+	  <br>
+	  <address>file-server-yoanndelattre.herokuapp.com</address>
+  </body>
+</html>
