ca1c442249a28d74ca26ad520b02df1351a5e416
yoanndelattre <yoanndelattre21@gmail.com>
2018-10-06 10:27:36 +0200

    add files

--- files/Projet Docker/DockerHub_Dockerfile_WebDav/DockerHub_Dockerfile_WebDav.php
+++ files/Projet Docker/DockerHub_Dockerfile_WebDav/DockerHub_Dockerfile_WebDav.php
@@ -3,20 +3,21 @@
   <head>
     <meta charset="utf-8">
     <title>Index of /Projet Docker/DockerHub_Dockerfile_WebDav/</title>
+    <link rel="shortcut icon" href="/img/logo.ico">
   </head>
   <body>
     <h1>Index of /Projet Docker/DockerHub_Dockerfile_WebDav/</h1>
 	  <table>
-		  
+
 		  <tr><th valign="top"><img src="/img/blank.gif" alt="[ICO]"></th><th><a href="?C=N;O=D">Name</a></th></tr>
    		  <tr><th colspan="4"><hr></th></tr>
-		  
+
 		<tr><td valign="top"><img src="/img/home.gif" alt="[HOMEDIR]"></td><td><a href="/">Home Directory</a></td><td>&nbsp;</td></tr>
-		  
+
 		<tr><td valign="top"><img src="/img/back.gif" alt="[PARENTDIR]"></td><td><a href="/files/Projet Docker/projet docker.php">Parent Directory</a></td><td>&nbsp;</td></tr>
-		  
+
 		<tr><td valign="top"><img src="/img/unknown.gif" alt="[   ]"></td><td><a href="install.sh">install.sh</a></td></tr>
-		  
+
 	  </table>
 	  <br>
 	  <address>file-server-yoanndelattre.herokuapp.com</address>
