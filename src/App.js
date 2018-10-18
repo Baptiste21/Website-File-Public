71c75379322826f42cadc4649e059701802a404a
Yoann Delattre <yoanndelattre21@gmail.com>
2018-10-18 21:06:52 +0200

    delete useless span | add br

--- src/App.js
+++ src/App.js
@@ -12,20 +12,22 @@ const Home = () => (
 //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 const Docker = () => (
   <div>
-      <h1 className='title-docker'>Path <span>/Docker/</span></h1>
+      <h1 className='title-docker'>Path= /Docker/</h1>
       <ul>
         <div className='DockerHub_Dockerfile_WebDav'><li><img src={require('./img/folder.gif')} alt="[DIR]" className='img-DockerHub_Dockerfile_WebDav'/><Link to="/Docker/DockerHub_Dockerfile_WebDav">DockerHub_Dockerfile_WebDav</Link></li></div>
      </ul>
+     <br></br>
      <hr/>
   </div>
 );
 //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 const DockerHub_Dockerfile_WebDav = () => (
   <div>
-    <h1 className='title-DockerHub_Dockerfile_WebDav'>Path <span>/Docker/DockerHub_Dockerfile_WebDav/</span></h1>
+    <h1 className='title-DockerHub_Dockerfile_WebDav'>Path= /Docker/DockerHub_Dockerfile_WebDav/</h1>
       <ul>
         <div className='installsh'><li><img src={require('./img/file.gif')} alt="[FILE]" className='img-installsh'/>install.sh<a href='..\files\Docker\DockerHub_Dockerfile_WebDav\install.sh' download><div className='bouton-download'>Download</div></a><ReactCopyButtonWrapper text='https://file-public.herokuapp.com/files/Docker/DockerHub_Dockerfile_WebDav/install.sh'><button className='bouton-download-link'>Copy Download Link</button></ReactCopyButtonWrapper></li></div>
       </ul>
+      <br></br>
      <hr/>
   </div>
 );
@@ -49,11 +51,12 @@ class App extends Component {
     }
     return (
       <div className="App">
-      <h1 className='title-'>Path <span>/</span></h1>
+      <h1 className='title'>Path= /</h1>
         <ul>
           <div className='home'><li><img src={require('./img/home.gif')} alt="[HOMEDIR]" className='img-homedir'/><Link to="/">Home</Link></li></div>
           <div className='docker'><li><img src={require('./img/folder.gif')} alt="[DIR]" className='img-docker'/><Link to="/Docker">Docker</Link></li></div>
         </ul>
+        <br></br>
       <span className='info-create'><span className='text-info-create'>Created with ReactJS</span><img src={require('./img/reactjs-logo.png')} alt="[LOGO-REACTJS]" className='img-info-create'/></span>
         <Route path="/" component={Home}/>
         <Route path="/docker" component={Docker}/>
